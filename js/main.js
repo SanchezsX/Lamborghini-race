e6c599f9b5c26a9d0efa84321a68a26d2b7d76ab
SanchezsX <113286578+SanchezsX@users.noreply.github.com>
2023-11-27 22:42:25 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+  });
+
+  let map;
+
+async function initMap() {
+  
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
